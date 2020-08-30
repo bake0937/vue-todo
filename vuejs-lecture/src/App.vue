fef6f74458ba7740aed572e999bf2c7874dcc78e
bake0937 <sadayasu.info@gmail.com>
2020-08-30 14:06:19 +0900

    共通ヘッダーの修正

--- vuejs-lecture/src/App.vue
+++ vuejs-lecture/src/App.vue
@@ -2,21 +2,17 @@
   <v-app>
     <v-app-bar app color="primary" dark>
       <v-app-bar-nav-icon></v-app-bar-nav-icon>
-      <div class="d-flex align-center"></div>
-      <span>マイアドレス帳</span>
+      <v-toolbar-title>マイアドレス帳</v-toolbar-title>
       <v-spacer></v-spacer>
     </v-app-bar>
 
-    <v-main> </v-main>
+    <v-content> </v-content>
   </v-app>
 </template>
 
 <script>
 export default {
   name: "App",
-
-  components: {},
-
   data: () => ({
     //
   }),
