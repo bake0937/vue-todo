b11f910f0d1f0361eab55136561ea748b2d4a1d4
bake0937 <sadayasu.info@gmail.com>
2020-08-30 17:14:52 +0900

    メニューの開閉状態の管理 コンポーネントからのストアの利用

--- vuejs-lecture/src/App.vue
+++ vuejs-lecture/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <v-app>
     <v-app-bar app color="primary" dark>
-      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+      <v-app-bar-nav-icon @click.stop="openSideMenu"></v-app-bar-nav-icon>
       <v-toolbar-title>マイアドレス帳</v-toolbar-title>
       <v-spacer></v-spacer>
     </v-app-bar>
@@ -20,5 +20,10 @@ export default {
   data: () => ({
     //
   }),
+  methods: {
+    openSideMenu() {
+      this.$store.dispatch("toggleSideMenu");
+    },
+  },
 };
 </script>
