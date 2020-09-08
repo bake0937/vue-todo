c17f336d55e964507946ebd4a69374717124dfad
bake0937 <sadayasu.info@gmail.com>
2020-09-09 07:34:18 +0900

    Googleログイン機能を実装

--- vuejs-lecture/src/main.js
+++ vuejs-lecture/src/main.js
@@ -3,8 +3,21 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import vuetify from "./plugins/vuetify";
+import firebase from "firebase";
 
 Vue.config.productionTip = false;
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDxQ9tCu1Dq6dsyp8k8zzOfMhZ4vEtIn1Q",
+  authDomain: "my-address-pj-246f1.firebaseapp.com",
+  databaseURL: "https://my-address-pj-246f1.firebaseio.com",
+  projectId: "my-address-pj-246f1",
+  storageBucket: "my-address-pj-246f1.appspot.com",
+  messagingSenderId: "863283930586",
+  appId: "1:863283930586:web:033f93e270331112b04ce3",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 new Vue({
   router,
