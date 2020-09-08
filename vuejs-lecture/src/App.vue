7629685a83ef12dd4bba9716cad1202178f1e8a8
bake0937 <sadayasu.info@gmail.com>
2020-09-09 07:51:10 +0900

    ログインユーザーの取得機能を実装

--- vuejs-lecture/src/App.vue
+++ vuejs-lecture/src/App.vue
@@ -16,6 +16,7 @@
 </template>
 
 <script>
+import firebase from "firebase";
 import SideNav from "./components/SideNav";
 import { mapActions } from "vuex";
 
@@ -24,11 +25,18 @@ export default {
   components: {
     SideNav,
   },
+  created() {
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setLoginUser(user);
+      }
+    });
+  },
   data: () => ({
     //
   }),
   methods: {
-    ...mapActions(["toggleSideMenu"]),
+    ...mapActions(["toggleSideMenu", "setLoginUser"]),
   },
 };
 </script>
